312455f224685192558e4be4f556ca41056a6533
Milit Donda <militdonda3@gmail.com>
2024-11-28 17:48:59 +0000

    added an error handler

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ var app = express();
 //logger middleware
 app.use(morgan("short"));
 
+//error handler
+app.use(function(req, res) {
+    res.status(404);
+    res.send("File not found!");
+});
+
 app.listen(3000, function() {
  console.log("App started on port 3000");
 });
