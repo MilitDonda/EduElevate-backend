5bc6b163752e17d59092f2b93b3a71c8d4987bc7
Milit Donda <militdonda3@gmail.com>
2024-11-28 19:25:25 +0000

    installed mongodb and properties reader. Connected to MongoDB.

--- app.js
+++ app.js
@@ -3,8 +3,25 @@ var cors = require("cors");
 var morgan = require("morgan");
 var path = require("path");
 var app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
+
+let propertiesReader = require("properties-reader");
+let propertiesPath = path.resolve(__dirname, "conf/db.properties");
+let properties = propertiesReader(propertiesPath);
+
+let dbPprefix = properties.get("db.prefix");
+let dbUsername = encodeURIComponent(properties.get("db.user"));
+let dbPwd = encodeURIComponent(properties.get("db.pwd"));
+let dbName = properties.get("db.dbName");
+let dbUrl = properties.get("db.dbUrl");
+let dbParams = properties.get("db.params");
+
+const uri = dbPprefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
+
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+let db = client.db(dbName);
 
 //logger middleware
 app.use(morgan("short"));
